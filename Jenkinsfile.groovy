9bf08d7629760073d2018afc4bb46bd74eb70359
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-16 16:29:40 +0530

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,13 @@
+node("master"){
+  stage ('Build') {
+ 
+    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/simple-java-maven-app.git'
+    withMaven(
+        // Maven installation declared in the Jenkins "Global Tool Configuration"
+      maven: 'M3'){
+        
+      bat "mvn verify"
+      
+    }
+  }
+}
